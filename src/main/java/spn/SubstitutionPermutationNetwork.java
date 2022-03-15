4b4c92ddcc3ada62a6693de68368d24b61b614e8
Yannick Koller <47781849+PossessedC0bra@users.noreply.github.com>
2022-03-15 15:34:27 +0100

    SubstitutionPermutationNetwork: minor improvements

--- src/main/java/spn/SubstitutionPermutationNetwork.java
+++ src/main/java/spn/SubstitutionPermutationNetwork.java
@@ -3,13 +3,15 @@ package spn;
 public class SubstitutionPermutationNetwork {
 
     private final int roundCount = 4; // r
-    private final int substitutionLength = 4; // n
-    private final int permutationLength = 4; // m
+    private final int substitutionBlockLength = 4; // n
+    private final int substitutionBlockCount = 4; // m
 
-    private final int[] sBox = new int[] {0xE, 0x4, 0xD, 0x1, 0x2, 0xF, 0xB, 0x8, 0x3, 0xA, 0x6, 0xC, 0x5, 0x9, 0x0, 0x7};
+    private final int[] sBox = new int[] {0b1110, 0b0100, 0b1101, 0b0001, 0b0010, 0b1111, 0b1011, 0b1000, 0b0011, 0b0001, 0b0110, 0b1100, 0b0101, 0b1001, 0b0000, 0b0111};
+    private final int[] sBoxInverse = new int[sBox.length];
     private final int[] pBox = new int[] {0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15};
 
-    private final int keyLength = 32; // s
+    private final int totalKeyLength = 32; // s
+
     // private final int key = 0b0011_1010_1001_0100_1101_0110_0011_1111;
     // TODO ykl: key below is only used for testing
     private final int key = 0b0001_0001_0010_1000_1000_1100_0000_0000; // TODO ykl: make this configurable
@@ -19,6 +21,13 @@ public class SubstitutionPermutationNetwork {
     }
 
     public SubstitutionPermutationNetwork() {
+        initSBoxInverse();
+    }
+
+    private void initSBoxInverse() {
+        for (int i = 0; i < sBox.length; i++) {
+            sBoxInverse[sBox[i]] = i;
+        }
     }
 
     public int encrypt(int clearText) {
@@ -38,17 +47,17 @@ public class SubstitutionPermutationNetwork {
 
     public int substitute(int number) {
         int result = 0;
-        for (int i = substitutionLength - 1; i >= 0; i--) {
-            int extract = (number >>> substitutionLength * i) & 0b1111;
+        for (int i = 0; i < substitutionBlockCount; i++) {
+            int extract = (number >>> substitutionBlockLength * i) & ((1 << substitutionBlockLength) - 1);
             int substitution = sBox[extract];
-            result |= substitution << substitutionLength * i;
+            result |= substitution << substitutionBlockLength * i;
         }
         return result;
     }
 
     public int permute(int number) {
         int result = 0;
-        for (int i = substitutionLength * permutationLength - 1; i >= 0; i--) {
+        for (int i = 0; i < substitutionBlockLength * substitutionBlockCount; i++) {
             result |= ((number >>> i) & 1) <<  pBox[i];
         }
         return result;
